5b9943e5367f0f29e8daf40fe0fa45564d6138b3
aniketsuryavanshi093 <aniketsuryavanshi093@gmail.com>
2024-04-14 23:47:49 +0530

    Schema update added

--- helpers/helper.go
+++ helpers/helper.go
@@ -56,6 +56,32 @@ func AuthMiddleware() gin.HandlerFunc {
 	}
 }
 
+func CheckAuth(c *gin.Context, token string) string {
+	if token == "" {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "No auth token provided"})
+		return ""
+	}
+
+	_token, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method")
+		}
+		return []byte("secret"), nil
+	})
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return ""
+	}
+	var userID = ""
+	if claims, ok := _token.Claims.(jwt.MapClaims); ok && _token.Valid {
+		userID = claims["user_id"].(string)
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+	return userID
+}
+
 func HashPassword(password string) (string, error) {
 
 	// Generate a salt
